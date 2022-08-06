db6ffb6a9c41acacf67277affc1603bf4a241620
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-06 14:48:42 -0700

    created backend routes and configured server to run on port 6000

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req, res) => {
+    res.status(200).json({message:"Get Goals"})
+})
+    router.post('/',(req, res) => {
+        res.status(200).json({message:"Set Goals"})
+  
+})
+router.put('/:id',(req, res) => {
+    res.status(200).json({message:`Update Goals ${req.params.id}`})
+
+})
+router.delete('/:id',(req, res) => {
+    res.status(200).json({message:`Delete Goals ${req.params.id}`})
+})
+
+module.exports = router
